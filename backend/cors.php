2a64c747fbb089755e3c2aa8ffc0bdaefa2f44ae
hdnanda <hariom.dipesh.nanda@gmail.com>
2025-04-23 10:48:57 +0530

    Update cors.php

--- backend/cors.php
+++ backend/cors.php
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     }
     
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
-        header("Access-Control-Allow-Headers: Content-Type, X-CSRF-Token, Authorization");
+        header("Access-Control-Allow-Headers: Content-Type, X-CSRF-Token, Authorization, Origin");
     }
     exit(0);
 }
