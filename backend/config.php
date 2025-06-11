57f0d1ec7681e33e19afe18d53da66bc26e5d663
hdnanda <hariom.dipesh.nanda@gmail.com>
2025-06-11 16:05:32 +0530

    Update config.php

--- backend/config.php
+++ backend/config.php
@@ -1,4 +1,10 @@
 <?php
+// --- START DEBUG ---
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+// --- END DEBUG ---
+
 // Start session if not already started
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
